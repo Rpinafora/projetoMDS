247cd867db92eb1de290daa95652e774dbd1c6ed
Rpinafora <rafaelfora96@gmail.com>
2017-05-26 13:54:09 +0100

    Add projeto

--- Prototipo Não Funcional MDS/Modelo.Designer.cs
+++ Prototipo Não Funcional MDS/Modelo.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\User\Desktop\projetoMDS\Prototipo Não Funcional MDS\Modelo.edmx'. 
+﻿// T4 code generation is enabled for model 'F:\IPL\S2\Metodologia de Desenvolvimento de Software\ProjetoMDS\Prototipo Não Funcional MDS\Modelo.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
